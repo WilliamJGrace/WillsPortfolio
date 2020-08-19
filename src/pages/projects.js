ba1ea6a75a22d28312158adbdffa605207104f93
WilliamJGrace <williamjgrace@outlook.com>
2020-08-19 12:20:40 +0100

    header links to project page

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from '../components/layout'
+
+const Projects = ({data}) => {
+    return (
+     <Layout>
+    <h2>hi</h2>
+    </Layout>
+    )
+  }
+  
+export default Projects
