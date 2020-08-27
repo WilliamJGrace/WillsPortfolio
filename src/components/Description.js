a7c4b205b6e78cdd27393a00bf1b12f2b7b8e679
WilliamJGrace <williamjgrace@outlook.com>
2020-08-27 17:39:48 +0100

    basic layout done looks poor

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,13 +1,20 @@
 import React from "react"
 import Grid from '@material-ui/core/Grid'
+import ProfilePic from '../components/profilePic'
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
     gridContainer: {
-      marginTop: '20px'},
+      marginTop: '100px',
+    marginBottom: '100px'},
 
     gridItem: {
-        textAlign: 'center'
+        textAlign: 'center',
+        margin: 'auto',
+        paddingLeft: '5px'
+    },
+
+    header: {
     }
   
     
@@ -20,11 +27,11 @@ const Description = () => {
     return (
         <Grid className={classes.gridContainer} container>
             <Grid item className={classes.gridItem} xs={6}> 
-         Hi
+        <ProfilePic/>
      </Grid>
      <Grid item className={classes.gridItem}xs={6}>
-         <h2>About Me</h2>
-         <div>I am a recent graduate of Makers Academy, seeking a Full Stack or Front End Software Developer role. Since graduating with a BSc in Microbiology from the University of Leeds, I gained professional experience with Anthony Nolan as a research assistant in a genetic lab in London. However, I didn’t see myself spending the rest of my life in a lab coat. I wanted more out of a career, so I began learning to code. I started a client-facing role in a fast-growing start-up in London to gain a better insight into the tech industry, whilst introducing myself to the basics of languages such as HTML, CSS and Javascript in my free time. I switched to software development because, fundamentally, I enjoy solving problems, learning new things and I want a career that challenges me, and allows me to create and contribute to exciting projects. Currently, I am building my client-facing, project management, and development skills volunteering at Mind.</div>
+         <h2 className={classes.header}>About Me</h2>
+         <div>I a BSc in Microbiology assistant in a genetic lab in London. However, I dg at Mind.</div>
 
      </Grid>
 </Grid>
--- src/components/profilePic.js
+++ src/components/profilePic.js
@@ -6,25 +6,25 @@ import { useStaticQuery, graphql } from "gatsby"
 
 
 const ProfilePic = () => {
-//     const data = useStaticQuery(graphql`
-// query MyQuery {
-//     file(relativePath: {eq: "ProfilePic.jpg"}) {
-//       childImageSharp {
-//         fluid {
-//           aspectRatio
-//           base64
-//           sizes
-//           src
-//         }
-//       }
-//     }
-//   }
-// `  
+    const data = useStaticQuery(graphql`
+query PhotoQuery {
+    file(relativePath: {eq: "ProfilePic.png"}) {
+      childImageSharp {
+        fluid {
+          aspectRatio
+          base64
+          sizes
+          src
+        }
+      }
+    }
+  }
+`  
 
-    // )
-    return 
-        // <Img fluid={data.file.childImageSharp.fluid}
-        // alt="A corgi smiling happily"></Img>
+    )
+    return (
+        <Img fluid={data.file.childImageSharp.fluid}
+        alt="A corgi smiling happily"></Img>)
     
 }
 
